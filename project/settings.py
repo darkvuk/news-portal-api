7e5cb26150d0019ef2a871f698a25aa479601671
darkvuk <dark.vukasovic@gmail.com>
2024-04-28 21:16:50 +0200

    Create User API

--- project/settings.py
+++ project/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -140,3 +144,4 @@ AUTH_USER_MODEL = 'user.User'
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_CREDENTIALS = True
+
--- user/views.py
+++ user/views.py
@@ -1,22 +1,31 @@
-from django.shortcuts import render
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import status
 from .serializers import UserSerializer
 from .models import User
+from .mixins import UserPermissionMixin
 import jwt
 import datetime
 
 
-class RegisterView(APIView):
+class RegisterView(UserPermissionMixin, APIView):
+
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        if self.is_superuser(request):
+            serializer = UserSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response({'detail': 'You don\'t have access permission.'},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
 
 
 class LoginView(APIView):
+
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
@@ -29,7 +38,7 @@ class LoginView(APIView):
             raise AuthenticationFailed('Incorect password.')
 
         payload = {
-            'id': user.email,
+            'id': user.user_id,
             'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=60),
             'iat': datetime.datetime.now(datetime.timezone.utc)
         }
@@ -45,8 +54,9 @@ class LoginView(APIView):
 
         return response
 
-class UserView(APIView):
 
+class UserView(APIView):
+    
     def get(self, request):
         token = request.COOKIES.get('jwt')
 
@@ -58,12 +68,14 @@ class UserView(APIView):
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed('Unauthenticated')
 
-        user = User.objects.filter(email=payload['id']).first()
+        user = User.objects.filter(user_id=payload['id']).first()
         serializer = UserSerializer(user)
 
         return Response(serializer.data)
 
+
 class LogoutView(APIView):
+
     def post(self, request):
         response = Response()
         response.delete_cookie('jwt')
@@ -71,3 +83,60 @@ class LogoutView(APIView):
             "message": "Success."
         }
         return response
+    
+
+class UserList(UserPermissionMixin, APIView):
+
+    def get(self, request):
+        if self.is_superuser(request):
+            users = User.objects.all()
+            serializer = UserSerializer(users, many=True)
+            return Response(serializer.data)
+        return Response(
+            {'detail': 'You don\'t have access permission.'},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+    
+
+class UserDetail(UserPermissionMixin, APIView):
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        if self.is_owner(request, pk) or self.is_superuser(request):
+            user = self.get_object(pk)
+            serializer = UserSerializer(user, data=request.data)    # PATCH: add partial=True
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            
+        return Response(
+            {'detail': 'User ID does not match the one in the token'},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+        
+    def delete(self, request, pk):
+        if self.is_superuser(request):
+            user_to_delete = self.get_object(pk)
+            user_to_delete.delete()
+            return Response(
+                {'detail': 'User has been successfully deleted.'}, 
+                status=status.HTTP_204_NO_CONTENT
+            )
+        return Response(
+            {'detail': 'You don\'t have access permission.'},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+
+    
+
